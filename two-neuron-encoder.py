66f5351d352ec27981199e8285be9e14533da5a2
Colton Fox <46362482+coltfox@users.noreply.github.com>
2024-08-27 13:46:16 -0400

    Add comments

--- two-neuron-encoder.py
+++ two-neuron-encoder.py
@@ -10,24 +10,28 @@ N_STEPS = T / DT
 timesteps = np.arange(0, T, DT)
 inp = 3 * np.sin(timesteps)
 
+# Plot input signal
 plt.figure()
 plt.title("Input Signal")
 plt.xlabel("t")
 plt.ylabel("x")
 plt.plot(timesteps, inp)
 
-encoder_pos = RateEncoder(n_neurons=1)
-encoder_neg = RateEncoder(n_neurons=1, encoders=-1)
-
+# Separate positive and negative signals
 pos_signal = inp.copy()
 pos_signal[pos_signal < 0] = 0
 
 neg_signal = inp.copy()
 neg_signal[neg_signal > 0] = 0
 
+# Encode each
+encoder_pos = RateEncoder(n_neurons=1)
+encoder_neg = RateEncoder(n_neurons=1, encoders=-1)
+
 spikes_pos = encoder_pos.encode(pos_signal)
 spikes_neg = encoder_neg.encode(neg_signal)
 
+# Plot spikes
 plt.figure()
 plt.title("Spikes Positive")
 plt.xlabel("t")
