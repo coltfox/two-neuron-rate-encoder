97b14fa4d5f054996206d5c693a04801450213d9
Colton Fox <46362482+coltfox@users.noreply.github.com>
2024-08-27 15:33:32 -0400

    Use random normal dist as input and plot spikes using nengo method

--- two-neuron-encoder.py
+++ two-neuron-encoder.py
@@ -1,14 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from rate_encoder import RateEncoder
+from rate_encoder import RateEncoder, plot_spikes
 
 T = 20
 DT = 0.1
-N_STEPS = T / DT
+N_STEPS = int(T / DT)
 
 timesteps = np.arange(0, T, DT)
-inp = 3 * np.sin(timesteps)
+inp = np.random.normal(loc=-0.5, size=N_STEPS)
 
 # Plot input signal
 plt.figure()
@@ -28,20 +28,20 @@ neg_signal[neg_signal > 0] = 0
 encoder_pos = RateEncoder(n_neurons=1)
 encoder_neg = RateEncoder(n_neurons=1, encoders=-1)
 
-spikes_pos = encoder_pos.encode(pos_signal)
-spikes_neg = encoder_neg.encode(neg_signal)
+spikes_pos = encoder_pos.encode(pos_signal).reshape(-1, 1)
+spikes_neg = encoder_neg.encode(neg_signal).reshape(-1, 1)
 
 # Plot spikes
 plt.figure()
 plt.title("Spikes Positive")
 plt.xlabel("t")
 plt.ylabel("S")
-plt.plot(timesteps, spikes_pos)
+plot_spikes(timesteps, spikes_pos)
 
 plt.figure()
 plt.title("Spikes Negative")
 plt.xlabel("t")
 plt.ylabel("S")
-plt.plot(timesteps, spikes_neg)
+plot_spikes(timesteps, spikes_neg)
 
 plt.show()
